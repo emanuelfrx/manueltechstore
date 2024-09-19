d1113804f8977fe0ea3047345d9af8dc28984cb4
Emanuel Freitas <161616695+emanuelfrx@users.noreply.github.com>
2024-09-19 16:58:45 +0000

    aaaa

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 // Baixe a chave privada JSON do Firebase Console em Configurações -> Contas de serviço
 const serviceAccount = require("./minhaChave2.json"); // O caminho do arquivo JSON
-
+//meu Deus nao acredito que deu certo
 // Inicializando o Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
